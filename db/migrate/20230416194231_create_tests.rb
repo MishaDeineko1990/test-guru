546d98c2c83ae332cfef1320dfcccc38007ad8af
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2023-04-16 23:43:23 +0300

    Add Rails models and database-level integrity constraints
    This commit adds Rails models for Test, Question, Answer, User, and Category entities, along with database-level integrity constraints to ensure required attributes cannot contain null values. Additionally, default values have been added using the migrations mechanism for the test level and the correct attribute of the Response.

---
+++ db/migrate/20230416194231_create_tests.rb
@@ -0,0 +1,13 @@
+class CreateTests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tests do |t|
+      t.string :name
+      t.integer :level
+      t.integer :time_limit
+      t.references :category, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230416202039_add_default_level_to_tests.rb
@@ -0,0 +1,5 @@
+class AddDefaultLevelToTests < ActiveRecord::Migration[6.1]
+  def change
+    change_column :tests, :level, :integer, :default => 0
+  end
+end
