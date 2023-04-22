aa33f2282e250ce3c338d812d880bcc5f941a71d
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2023-04-22 17:06:14 +0300

    Add seed data and ActiveRecord methods
    Added seed data to db/seeds.rb file for all existing models. Also created a new instance method on the User model that takes in a difficulty value as an argument and returns a list of all Tests passed or ever passed at that level. Additionally, added a class method to the Test model that returns an array of names of all Tests sorted in descending order by Category name matching a certain criteria provided as an argument.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
-  belongs_to :category
   belongs_to :user
+  belongs_to :category
+  has_many :questions
+  
+  def self.sorted_test_names(category_name)
+    self.joins(:category).where(categories: {name: category_name}).order(name: :desc).pluck(:name)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :tests
+
+	def passed_tests(difficulty)
+		self.tests.select { |test| (test.passed && test.level == difficulty) }
+	end
 end
